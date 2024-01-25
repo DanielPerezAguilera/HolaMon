37bf7af6232c2332f27f94d21916743909592e88
Daniel Perez Aguilera <dpa01@iesemilidarder.com>
2024-01-25 20:14:47 +0100

    Primera  version

---
+++ Holamundo2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.edproject;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Holamundo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello big World!");
+    }
+}
+
+
